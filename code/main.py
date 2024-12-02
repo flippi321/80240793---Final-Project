a94dab6a9c247011dc3c8c058fbcef6c32905c75
flippi321 <90700334+flippi321@users.noreply.github.com>
2024-12-02 18:59:27 +0800

    Added a main to run the code

--- code/main.py
+++ code/main.py
@@ -1,1 +1,12 @@
-print("Hello World")
+from GAN import BDI_GAN
+
+# Running the code
+model = BDI_GAN()
+
+print("Building Model...")
+build_gan = model.build_model()
+print("Done :-) \n")
+
+# We train the model for 10000 epochs
+print("Training Model...")
+model.train_model(epochs=10000, batch_size=32, save_interval=50, input_dir="data/monet_jpg", print_interval=10)
