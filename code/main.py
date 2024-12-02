b94cc9f355b3ba985189dcdb462127358d150931
flippi321 <90700334+flippi321@users.noreply.github.com>
2024-12-03 03:14:40 +0800

    Reduced learning rate to work better with updated GAN

--- code/main.py
+++ code/main.py
@@ -1,17 +1,17 @@
 from GAN import BDI_GAN
 
 # Running the code
-generator_lr = 5e-2
-discriminator_lr = 5e-2
+generator_lr = 5e-4
+discriminator_lr = 5e-4
 model = BDI_GAN(g_lr=generator_lr, d_lr=discriminator_lr)
 
-build_gan = model.build_model(show_summary=False)
+build_gan = model.build_model(show_summary=True)
 
 # Train model
 model.train_model(
     epochs=10000, 
     batch_size=32, 
-    output_image_interval = 50, 
+    output_image_interval = 100, 
     save_model_interval = 1000, 
     input_dir="data/monet_jpg", 
     output_dir="output", 
