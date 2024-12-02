7ec62a5ba966d385fc4869310020486bf323de9b
flippi321 <90700334+flippi321@users.noreply.github.com>
2024-12-02 21:13:58 +0800

    Added save_model_interval to the model training

--- code/main.py
+++ code/main.py
@@ -3,10 +3,14 @@ from GAN import BDI_GAN
 # Running the code
 model = BDI_GAN()
 
-print("Building Model...")
-build_gan = model.build_model()
-print("Done :-) \n")
+build_gan = model.build_model(show_summary=True)
 
-# We train the model for 10000 epochs
-print("Training Model...")
-model.train_model(epochs=10000, batch_size=32, save_interval=50, input_dir="data/monet_jpg", print_interval=10)
+# Train model
+model.train_model(
+    epochs=10000, 
+    batch_size=64, 
+    output_image_interval = 50, 
+    save_model_interval = 1000, 
+    input_dir="data/monet_jpg", 
+    output_dir="output", 
+    print_interval=10)
